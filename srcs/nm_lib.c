7802dcf6911e0c510c9c6359198fd7b587907ab3
Sébastien Nicolet <snicolet@student.42.fr>
2018-02-26 14:16:19 +0100

    some imrpovements

--- srcs/nm_lib.c
+++ srcs/nm_lib.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/18 13:16:28 by snicolet          #+#    #+#             */
-/*   Updated: 2018/02/25 07:52:49 by snicolet         ###   ########.fr       */
+/*   Updated: 2018/02/25 16:27:12 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,14 @@ static int	load_ar(const struct ar_hdr *ar, t_ar *load)
 	success += ft_sscanf(ar->ar_gid, "%u", &load->gid);
 	success += ft_sscanf(ar->ar_mode, "%x", &load->mode);
 	success += ft_sscanf(ar->ar_size, "%lu", &load->size);
-	ft_printf("%s%s%s%lu%s%u%s%u%s%x%s%lu - final: %d\n",
-		"name: ", load->name,
-		" date: ", load->date,
-		" uid: ", load->uid,
-		" gid: ", load->gid,
-		" mode: ", load->mode,
-		" size: ", load->size, success
-	);
+	// ft_printf("%s%s%s%lu%s%u%s%u%s%x%s%lu - final: %d\n",
+	// 	"name: ", load->name,
+	// 	" date: ", load->date,
+	// 	" uid: ", load->uid,
+	// 	" gid: ", load->gid,
+	// 	" mode: ", load->mode,
+	// 	" size: ", load->size, success
+	// );
 	return (success == 5);
 }
 
@@ -39,17 +39,18 @@ static void	lib_rl(struct ranlib *rl, const size_t size, t_nm *nm)
 {
 	size_t				index;
 	struct ar_hdr		*ar;
-	const char			*name;
+	t_ar				load;
 
 	nm->flags |= NM_FLAG_SHOWNAME | NM_FLAG_SYMTAB;
 	index = 0;
 	while (index < size)
 	{
 		ar = (void*)&nm->rootraw[rl->ran_off];
-		name = (void*)((size_t)ar + sizeof(*ar));
-		nm->filepath = name;
-		nm->fileraw = (void*)((size_t)name + ft_strlen(name) + 4);
+		load_ar(ar, &load);
+		nm->filepath = (char*)((size_t)ar + sizeof(*ar));
+		nm->fileraw = (char*)((size_t)nm->filepath + ft_strlen(nm->filepath) + 4);
 		nm->magic = *(unsigned int *)(size_t)nm->fileraw;
+		nm->filesize = load.size;
 		handle_files_types(nm);
 		rl++;
 		index += 8;
