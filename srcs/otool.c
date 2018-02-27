3b9f19bd06663c531f955b100d7c6254032971b8
Sébastien Nicolet <snicolet@student.42.fr>
2018-02-27 23:35:44 +0100

    a start for the otool part

---
+++ srcs/otool.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   otool.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/27 23:08:07 by snicolet          #+#    #+#             */
+/*   Updated: 2018/02/27 23:34:44 by snicolet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "otool.h"
+#include <sys/mman.h>
+
+static void		otool_showmem(const unsigned char *ptr,
+	const size_t size, const size_t offset)
+{
+	size_t			pos;
+
+	pos = 0;
+	while (pos < size)
+	{
+		ft_printf("%p\t\t%02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x"
+			" %02x %02x %02x %02x %02x\n",
+			pos + offset, ptr[0], ptr[1], ptr[2], ptr[3], ptr[4], ptr[5], ptr[6],
+			ptr[7], ptr[8], ptr[9], ptr[10], ptr[11], ptr[12], ptr[13], ptr[14],
+			ptr[15]);
+		pos += 16;
+		ptr += 16;
+	}
+}
+
+static int		otool_run(const char *filepath, const int index, const int max)
+{
+	char		*fileraw;
+	size_t		filesize;
+
+	fileraw = loadfile(filepath, &filesize);
+	if (!fileraw)
+	{
+		ft_dprintf(STDERR_FILENO, "%s%s\n", "error: failed to open: ",
+			filepath);
+		return (EXIT_FAILURE);
+	}
+	if (max > 1)
+		ft_printf("%s:\n", filepath);
+	(void)index;
+	otool_showmem((unsigned char*)(size_t)fileraw, filesize, 0);
+	munmap(fileraw, filesize);
+	return (EXIT_SUCCESS);
+}
+
+int		main(int ac, char **av)
+{
+	int			p;
+	int			retcode;
+
+	if (ac < 2)
+	{
+		ft_dprintf(STDERR_FILENO, "%s", "error: missing file path\n");
+		return (EXIT_FAILURE);
+	}
+	retcode = EXIT_SUCCESS;
+	p = 1;
+	while (p < ac)
+	{
+		retcode += otool_run(av[p], p, ac - 1);
+		p++;
+	}
+	return (retcode);
+}
