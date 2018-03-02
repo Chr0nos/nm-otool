4b538463e08319fedc187a50972c88e016edfd34
Sébastien Nicolet <snicolet@student.42.fr>
2018-03-02 23:57:28 +0100

    cleans

--- tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-make
-echo "starting tests"
-diff <(./ft_nm bin32bits) <(nm bin32bits)
-diff <(./ft_nm libft/build/string/ft_strsplit.o) <(nm libft/build/string/ft_strsplit.o)
-diff <(./ft_nm ../scope/scop) <(nm ../scope/scop)
-diff <(./ft_nm /usr/local/bin/python3) <(nm /usr/local/bin/python3)
-echo "done"
