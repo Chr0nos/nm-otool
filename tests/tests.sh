24c852085c1875e4695ab1e8f5c45f5cd9e77b28
Sébastien Nicolet <snicolet@student.42.fr>
2018-03-03 02:23:54 +0100

    fixed test

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 make
 echo "starting tests"
-diff <(./ft_nm bin32bits) <(nm bin32bits)
+diff <(./ft_nm tests/bin32bits) <(nm tests/bin32bits)
 diff <(./ft_nm libft/build/string/ft_strsplit.o) <(nm libft/build/string/ft_strsplit.o)
 diff <(./ft_nm ../scope/scop) <(nm ../scope/scop)
 diff <(./ft_nm /usr/local/bin/python3) <(nm /usr/local/bin/python3)
