245f677778c1d66ac8d5bfdaa1840e7c1ceefcb8
Chr0nos <snicolet@student.42.fr>
2018-03-03 01:07:56 +0000

    added some comments to common

--- include/common.h
+++ include/common.h
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/02 22:42:51 by snicolet          #+#    #+#             */
-/*   Updated: 2018/03/02 23:34:58 by snicolet         ###   ########.fr       */
+/*   Updated: 2018/03/03 01:07:24 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,21 @@
 # define COMMON_H
 # include "libft.h"
 
-typedef struct	s_common
+/*
+** -------------------------------------------------------------------------- **
+** flags    : binaries flags defined into flags.h
+** filesize : the current file size, it can be a sub size (ex: fat binaries)
+** filepath : the full path of the file
+** fileraw  : the current root of the file, can be a sub content (fat bin)
+** segments : segments list, can be segments in 32 or 64 bits
+** rootraw  : the real root raw data, never changes, used in security with rfs
+** rfs      : meaning for "Real File Size", the full size of rootraw
+** -------------------------------------------------------------------------- **
+** note: this structure has been designed to be accesible from a (t_otool)
+** and a (t_nm*) with a cast like (t_common*)nm or (t_common*)otool
+*/
+
+typedef struct		s_common
 {
 	size_t			flags;
 	size_t			filesize;
@@ -24,7 +38,7 @@ typedef struct	s_common
 	unsigned char	*rootraw;
 	size_t			rfs;
 	void			*padding;
-}				t_common;
+}					t_common;
 
 # define SEGSIZE64	sizeof(struct segment_command_64)
 # define SEGSIZE32	sizeof(struct segment_command)
