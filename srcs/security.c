bdc35af73ce700ee974de04695826c9c606cb813
Sébastien Nicolet <snicolet@student.42.fr>
2018-03-03 14:33:04 +0100

    added the file path into a corrupted message information error

--- srcs/security.c
+++ srcs/security.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/02 19:38:20 by snicolet          #+#    #+#             */
-/*   Updated: 2018/03/03 03:08:41 by snicolet         ###   ########.fr       */
+/*   Updated: 2018/03/03 14:32:02 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,8 @@ size_t		security(t_common *com, const void *ptr, const size_t size)
 	if  ((ptr < (const void*)fileraw) || (ptr > (const void*)&fileraw[rawsize]))
 	{
 		com->flags |= FLAG_ERROR;
-		ft_dprintf(STDERR_FILENO, "%s\n", "error: the file is corrupted.");
+		ft_dprintf(STDERR_FILENO, "%s%s%s\n",
+			"error: the file \"", com->filepath, "\" is corrupted.");
 		return (FLAG_ERROR);
 	}
 	return (FLAG_OK);
