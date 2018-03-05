4b7393c1820cf49a84a99a76a25fc66afa59ba4f
Sebastien NICOLET <snicolet@student.42.fr>
2018-03-05 14:41:21 +0100

    improved security in file detection with correct sizes instead of just the size of an unsigned int

--- include/filetype.h
+++ include/filetype.h
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/03 04:05:51 by snicolet          #+#    #+#             */
-/*   Updated: 2018/03/05 14:28:59 by snicolet         ###   ########.fr       */
+/*   Updated: 2018/03/05 14:40:58 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,29 +20,64 @@
 
 # pragma pack(push, 1)
 
-typedef struct		s_filehandler
+typedef struct				s_filehandler
 {
-	const char		*desc;
-	unsigned int	magic;
-	size_t			flags;
-	size_t			minsize;
-}					t_filehandler;
+	const char				*desc;
+	unsigned int			magic;
+	size_t					flags;
+	size_t					minsize;
+}							t_filehandler;
 
 # pragma pack(pop)
 
-# define TFH t_filehandler
 # define HANDLERS_COUNT 7
 
-static const TFH	g_handlers[HANDLERS_COUNT] = {
-	(TFH){"binary 64bits", MH_MAGIC_64, FLAG_64BITS | FLAG_MACHO, 4},
-	(TFH){"cig 64bits", MH_CIGAM_64, FLAG_64BITS | FLAG_MACHO | FLAG_CIGAM, 4},
-	(TFH){"binary 32bits", MH_MAGIC, FLAG_32BITS | FLAG_MACHO, 4},
-	(TFH){"cigam 32bits", MH_CIGAM, FLAG_32BITS | FLAG_MACHO | FLAG_CIGAM, 4},
-	(TFH){"fat bin 32 bits", FAT_MAGIC, FLAG_32BITS | FLAG_FAT, 4},
-	(TFH){"ccig 32 bits", FAT_CIGAM, FLAG_32BITS | FLAG_FAT | FLAG_CIGAM, 4},
-	(TFH){"fcig 64 bits", FAT_MAGIC_64, FLAG_64BITS | FLAG_FAT | FLAG_CIGAM, 4},
+static const t_filehandler	g_handlers[HANDLERS_COUNT] = {
+	(t_filehandler){
+		"binary 64bits",
+		MH_MAGIC_64,
+		FLAG_64BITS | FLAG_MACHO,
+		sizeof(unsigned int) + sizeof(struct load_command)
+	},
+	(t_filehandler){
+		"binary 64bits (cigam)",
+		MH_CIGAM_64,
+		FLAG_64BITS | FLAG_MACHO | FLAG_CIGAM,
+		sizeof(unsigned int) + sizeof(struct load_command)
+	},
+	(t_filehandler){
+		"binary 32bits",
+		MH_MAGIC,
+		FLAG_32BITS | FLAG_MACHO,
+		sizeof(unsigned int) + sizeof(struct load_command)
+	},
+	(t_filehandler){
+		"binary 32bits",
+		MH_CIGAM,
+		FLAG_32BITS | FLAG_MACHO | FLAG_CIGAM,
+		sizeof(unsigned int) + sizeof(struct load_command)
+	},
+	(t_filehandler){
+		"fat binary 32 bits",
+		FAT_MAGIC,
+		FLAG_32BITS | FLAG_FAT,
+		sizeof(unsigned int) + sizeof(struct fat_header)
+	},
+	(t_filehandler){
+		"fat binary 32 bits (cigam)",
+		FAT_CIGAM,
+		FLAG_32BITS | FLAG_FAT | FLAG_CIGAM,
+		sizeof(unsigned int) + sizeof(struct fat_header)
+	},
+	(t_filehandler){
+		"fat binary 64 bits (cigam)",
+		FAT_MAGIC_64,
+		FLAG_64BITS | FLAG_FAT | FLAG_CIGAM,
+		sizeof(unsigned int) + sizeof(struct fat_header)
+	},
 };
 
-size_t				filetype(const char *fileraw, const size_t filesize);
+size_t						filetype(const char *fileraw,
+	const size_t filesize);
 
 #endif
